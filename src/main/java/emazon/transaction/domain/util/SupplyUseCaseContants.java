6280eb8661071f6deb4872b4e65b53c5bdfabf61
Manuel Carrascal <tricdiart@gmail.com>
2024-09-27 22:30:50 -0500

    feat: Add constants for purchasing logic.

--- src/main/java/emazon/transaction/domain/util/SupplyUseCaseContants.java
+++ src/main/java/emazon/transaction/domain/util/SupplyUseCaseContants.java
@@ -2,7 +2,7 @@ package emazon.transaction.domain.util;
 
 public class SupplyUseCaseContants {
     public static final String PRODUCT_NOT_FOUND = "Product not found";
-
+    public static final String NEXT_SUPPLY_DATE_REQUIRED = "Next supply date is required";
     private SupplyUseCaseContants() {
     }
 }
